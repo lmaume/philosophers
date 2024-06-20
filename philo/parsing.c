b9f1a4ee0f68a56797f3e564d93288ace1096188
Lucas Maume <lmaume@k0r2p11.42mulhouse.fr>
2024-06-20 13:43:25 +0200

    philo 0.1.1 (to the norm)

--- philo/parsing.c
+++ philo/parsing.c
@@ -6,7 +6,7 @@
 /*   By: lmaume <lmaume@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/20 11:12:33 by lmaume            #+#    #+#             */
-/*   Updated: 2024/06/20 12:00:57 by lmaume           ###   ########.fr       */
+/*   Updated: 2024/06/20 13:42:49 by lmaume           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,9 +16,9 @@ bool	is_entry_valid(int argc, char **argv)
 {
 	if (argc < 5 || ft_atoi(argv[1], NULL) < 1)
 	{
-		printf("false\n");	
+		printf("false\n");
 		return (false);
 	}
 	printf("true");
-	return true;
+	return (true);
 }
--- philo/philosophers.h
+++ philo/philosophers.h
@@ -6,25 +6,25 @@
 /*   By: lmaume <lmaume@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/20 11:04:46 by lmaume            #+#    #+#             */
-/*   Updated: 2024/06/20 11:57:24 by lmaume           ###   ########.fr       */
+/*   Updated: 2024/06/20 13:42:24 by lmaume           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PHILOSOPHERS_H
 # define PHILOSOPHERS_H
 
-#include <unistd.h>
-#include <stdlib.h>
-#include <stdio.h>
-#include <string.h>
-#include <sys/time.h>
-#include <pthread.h>
-#include <stdbool.h>
+# include <unistd.h>
+# include <stdlib.h>
+# include <stdio.h>
+# include <string.h>
+# include <sys/time.h>
+# include <pthread.h>
+# include <stdbool.h>
 
 // ? BASE FUNCTIONS (LIB SUBSTITUTE) :
 size_t	ft_strlen(const char *str);
-int	ft_strncmp(const char *s1, const char *s2, size_t n);
-int	ft_atoi(char *nptr, int *error);
+int		ft_strncmp(const char *s1, const char *s2, size_t n);
+int		ft_atoi(char *nptr, int *error);
 
 // ? PARSING FUNCTIONS :
 bool	is_entry_valid(int argc, char **argv);
